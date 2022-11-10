f077464ad1381b53a7d07dc708488b38e814c5bf
Jussi Mäki-Ikola <jussimakiikola@gmail.com>
2022-11-10 22:21:51 +0200

    local mongodb working with backend

--- backend/database/database.go
+++ backend/database/database.go
@@ -12,10 +12,18 @@ import (
 )
 
 func NewDatabaseConnection() *mongo.Client {
-	fmt.Printf("Using db credentials: %s:%s\n", os.Getenv("MINIGOLF_DB_USER"), os.Getenv("MINIGOLF_DB_PASSWORD"))
+	protocol := os.Getenv("MINIGOLF_DB_PROTOCOL")
+	user := os.Getenv("MINIGOLF_DB_USER")
+	password := os.Getenv("MINIGOLF_DB_PASSWORD")
+	host := os.Getenv("MINIGOLF_DB_HOST")
+	url := fmt.Sprintf("%s://%s:%s@%s/?retryWrites=true&w=majority", protocol, user, password, host)
+
+	fmt.Printf("MONGO_URL: %s \n", url)
+
 	serverAPIOptions := options.ServerAPI(options.ServerAPIVersion1)
+
 	clientOptions := options.Client().
-		ApplyURI(fmt.Sprintf("mongodb+srv://%s:%s@minigolf-mongo-atlas.nxtkiht.mongodb.net/?retryWrites=true&w=majority", os.Getenv("MINIGOLF_DB_USER"), os.Getenv("MINIGOLF_DB_PASSWORD"))).
+		ApplyURI(url).
 		SetServerAPIOptions(serverAPIOptions)
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
