f122b778f21f287f261e71b1e11fb93853619c4f
Jussi Mäki-Ikola <jussimakiikola@gmail.com>
2022-11-13 15:26:51 +0200

    Render stored gamemaps

--- frontend/src/pages/Root.tsx
+++ frontend/src/pages/Root.tsx
@@ -1,15 +1,29 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Row from '../components/Row';
+import { GameMap } from '../types';
+import { BASE_URL } from '../utils/api';
+import MapController from '../controllers/MapController';
 
 const RootPage: React.FC = () => {
   const [gameId, setGameId] = useState('');
   const [name, setName] = useState('');
   const [error, setError] = useState('');
+  const [maps, setMaps] = useState<GameMap[]>([]);
   const navigate = useNavigate();
 
   useEffect(() => {
     setError('');
   }, [name, gameId]);
+
+  useEffect(() => {
+    const fetchMaps = async () => {
+      const response = await fetch(`http://${BASE_URL}/api/game-maps`);
+      const data = await response.json();
+      setMaps(data);
+    };
+    fetchMaps();
+  }, []);
 
   const handleNavigate = () => {
     if (name && gameId) {
@@ -52,6 +66,23 @@ const RootPage: React.FC = () => {
           Liity
         </button>
       </p>
+
+      {maps.map((m) => {
+        // TODO: The images should probably be set in state.
+        const canvas = document.createElement('canvas');
+        const controller = new MapController(canvas);
+        controller.setGameMap(m);
+        controller.init();
+        const imageSrc = canvas.toDataURL();
+        return (
+          <Row key={m.id}>
+            <div className='column'>
+              <h2>Kartta {m.id}</h2>
+              <img src={imageSrc} width={400}></img>
+            </div>
+          </Row>
+        );
+      })}
     </div>
   );
 };
