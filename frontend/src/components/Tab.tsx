db2224563cbea6a078bee605aa574a05f2b63f62
Jussi Mäki-Ikola <jussimakiikola@gmail.com>
2022-11-20 23:39:22 +0200

    Set tab to url-param

--- frontend/src/components/Tab.tsx
+++ frontend/src/components/Tab.tsx
@@ -1,43 +1,55 @@
-import { useState } from "react";
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
 
 export type Tab = {
   label: string;
   default?: boolean;
   view: React.ReactNode;
-}
+};
 
 type TabGroupProps = {
   tabs: Tab[];
-}
+};
 
-export const TabGroup: React.FC<TabGroupProps> = (props: TabGroupProps) => {
+export const TabGroup: React.FC<TabGroupProps> = ({ tabs }: TabGroupProps) => {
+  const [selected, setSelected] = useState<Tab>(tabs.find((tab) => tab.default) || tabs[0]);
+  const [params, setParams] = useSearchParams();
 
-  const [selected, setSelected] = useState<Tab>(props.tabs.find(tab => tab.default) || props.tabs[0])
-  const isSelected = (tab: Tab): boolean => { return tab === selected} 
+  const setTab = (idx: number) => {
+    setParams(idx > 0 ? { tab: `${idx}` } : {}, { replace: true });
+  };
+
+  const tabIdx = parseInt(params.get('tab') || '');
+
+  useEffect(() => {
+    if (isNaN(tabIdx)) {
+      setSelected(tabs.find((tab) => tab.default) || tabs[0]);
+      return;
+    }
+    setSelected(tabs[tabIdx]);
+  }, [tabIdx, tabs]);
+
+  const isSelected = (tab: Tab): boolean => {
+    return tab === selected;
+  };
   return (
-    <div className="flex flex-col drop-shadow-2xl">
-      <div className="flex flex-row pl-4">
-        {props.tabs.map(tab =>
-          <div key={tab.label} className="text-xl">
-            { isSelected(tab) ? (
-            <button 
-              className="rounded-t-xl bg-green-600 mx-1 py-1 px-4 "
-              onClick={() => setSelected(tab)}>
-                { tab.label }
-            </button>
+    <div className='flex flex-col drop-shadow-2xl'>
+      <div className='flex flex-row pl-4'>
+        {tabs.map((tab, idx) => (
+          <div key={tab.label} className='text-xl'>
+            {isSelected(tab) ? (
+              <button className='rounded-t-xl bg-green-600 mx-1 py-1 px-4 ' onClick={() => setTab(idx)}>
+                {tab.label}
+              </button>
             ) : (
-            <button 
-              className="rounded-t-xl bg-green-800 mx-1 py-1 px-4"
-              onClick={() => setSelected(tab)}>
-                { tab.label }
-            </button>
+              <button className='rounded-t-xl bg-green-800 mx-1 py-1 px-4' onClick={() => setTab(idx)}>
+                {tab.label}
+              </button>
             )}
           </div>
-        )}
+        ))}
       </div>
-      <div className="rounded-xl p-20 bg-green-600" >
-        { selected.view }
-      </div>
+      <div className='rounded-xl p-20 bg-green-600'>{selected.view}</div>
     </div>
-  )
+  );
 };
